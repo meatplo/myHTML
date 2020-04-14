7f9fc6875ace15fbb9801a3ce27c363f36fc653a
meatplo <1327233911@qq.com>
2020-04-14 09:53:40 +0800

    作业6 43-张倍瑞 finish post youdao

--- myproject/post_youdao.py
+++ myproject/post_youdao.py
@@ -1,9 +1,7 @@
 import random
 import requests
 import time
-
-# url = "http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
-# content = "我爱中国"
+import json
 
 
 class Youdao():
@@ -15,12 +13,7 @@ class Youdao():
         self.sign = self.get_sign()
 
     def get_salt(self):
-        s = random.randint(0, 10)
-        t = self.ts
-        # print("random=",s)
-        # print("ts=",t)
-        # print("salt=",t+str(s))
-        return t+str(s)
+        return self.ts + random.randint(0, 10)
 
     def get_md5(self, value):
         import hashlib
@@ -29,23 +22,16 @@ class Youdao():
         return m.hexdigest()
 
     def get_sign(self):
-        i = self.salt
-        e = self.content
-        s = "fanyideskweb" + e + i + "Nw(nmmbP%A-r6U3EUn]Aj"
-        # print("s=",s,"md5=",get_md5(s))
+        s = "fanyideskweb" + self.content + self.salt + "Nw(nmmbP%A-r6U3EUn]Aj"
         return self.get_md5(s)
-        # return '2a57c66e7e063f81f8b730a87a41514b'
 
     def get_ts(self):
         t = time.time()
-        ts = str(int(round(t*1000)))
-        return ts
+        return str(int(round(t * 1000)))
 
-    # def get_content(self):
-        # return '我爱中国'
 
     def yield_form_data(self):
-        form_data = {
+        return {
             'i': self.content,
             'from': 'AUTO',
             'to': 'AUTO',
@@ -60,21 +46,22 @@ class Youdao():
             'keyfrom': 'fanyi.web',
             'action': 'FY_BY_CLICKBUTTIONi',
         }
-        return form_data
 
-    def get_headers(self):
-        headers = {
+    def yieid_headers(self):
+        return {
             'Cookie': 'OUTFOX_SEARCH_USER_ID=552218048@10.108.160.19; OUTFOX_SEARCH_USER_ID_NCOO=1027920486.8660557; JSESSIONID=aaaSCabsZTj3hc01bLIfx; ___rl__test__cookies=1586498032499',
             'Referer': 'http://fanyi.youdao.com/',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
         }
-        return headers
 
     def fanyi(self):
-        response = requests.post(self.url, data=self.yield_form_data(), headers=self.get_headers())
-        return response.text
+        response = requests.post(self.url, data=self.yield_form_data(), headers=self.yieid_headers())
+        content = json.loads(response.text)
+        return content['translateResult'][0][0]['tgt']
 
 
 if __name__ == '__main__':
-    youdao = Youdao('我爱中国')
-    print(youdao.fanyi())
+    while(True):
+        i = input("please input :")
+        youdao = Youdao(i)
+        print("fanyi result :",youdao.fanyi())
